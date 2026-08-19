247319badb093c07a0c7d62a659ead7758e26d1e
Agtdbx <auguste.deroubaix@gmail.com>
2026-08-19 12:31:39 +0200

    error and warning handled, color added

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import random as rd
 import customtkinter as ctk
 
 from define import APP_TITLES, WINDOW_START_SIZE, WINDOW_MIN_SIZE
-from transformations import get_deck_changes
+from transformations import DeckParseError, parse_deck_list, get_deck_changes
 
 class DeckUpdaterApp(ctk.CTk):
     def __init__(self):
@@ -24,13 +24,19 @@ class DeckUpdaterApp(ctk.CTk):
         self.frame_old = ctk.CTkFrame(self)
         self.frame_old.grid(row=0, column=0, padx=10, pady=(10, 5), sticky="nsew")
         self.frame_old.grid_columnconfigure(0, weight=1)
-        self.frame_old.grid_rowconfigure(1, weight=1)
+        self.frame_old.grid_rowconfigure(2, weight=1)
 
         self.label_old = ctk.CTkLabel(self.frame_old, text="Old deck list", font=ctk.CTkFont(weight="bold"))
-        self.label_old.grid(row=0, column=0, pady=5)
+        self.label_old.grid(row=0, column=0, pady=(5, 0))
+
+        self.label_info_old = ctk.CTkLabel(self.frame_old, text="", text_color="red")
 
         self.textbox_old = ctk.CTkTextbox(self.frame_old)
-        self.textbox_old.grid(row=1, column=0, padx=10, pady=10, sticky="nsew")
+        self.textbox_old.grid(row=2, column=0, padx=10, pady=10, sticky="nsew")
+        self.textbox_old.bind("<Control-a>", lambda e: self.select_all(self.textbox_old))
+
+        self.btn_clear_old = ctk.CTkButton(self.frame_old, text="Clear", font=ctk.CTkFont(weight="bold"), command=lambda: self.clear_all(self.textbox_old))
+        self.btn_clear_old.grid(row=3, column=0, columnspan=1, padx=10, pady=10, sticky="ew")
 
         # ==========================================
         # New Deck
@@ -38,13 +44,19 @@ class DeckUpdaterApp(ctk.CTk):
         self.frame_new = ctk.CTkFrame(self)
         self.frame_new.grid(row=0, column=1, padx=10, pady=(10, 5), sticky="nsew")
         self.frame_new.grid_columnconfigure(0, weight=1)
-        self.frame_new.grid_rowconfigure(1, weight=1)
+        self.frame_new.grid_rowconfigure(2, weight=1)
 
         self.label_new = ctk.CTkLabel(self.frame_new, text="New deck list", font=ctk.CTkFont(weight="bold"))
-        self.label_new.grid(row=0, column=0, pady=5)
+        self.label_new.grid(row=0, column=0, pady=(5, 0))
+
+        self.label_info_new = ctk.CTkLabel(self.frame_new, text="", text_color="red")
 
         self.textbox_new = ctk.CTkTextbox(self.frame_new)
-        self.textbox_new.grid(row=1, column=0, padx=10, pady=10, sticky="nsew")
+        self.textbox_new.grid(row=2, column=0, padx=10, pady=10, sticky="nsew")
+        self.textbox_new.bind("<Control-a>", lambda e: self.select_all(self.textbox_new))
+
+        self.btn_clear_new = ctk.CTkButton(self.frame_new, text="Clear", font=ctk.CTkFont(weight="bold"), command=lambda: self.clear_all(self.textbox_new))
+        self.btn_clear_new.grid(row=3, column=0, columnspan=1, padx=10, pady=10, sticky="ew")
 
         # ==========================================
         # BOUTON
@@ -53,23 +65,57 @@ class DeckUpdaterApp(ctk.CTk):
         self.btn_changes.grid(row=1, column=0, columnspan=2, padx=10, pady=10, sticky="ew")
 
 
+    def select_all(self, textbox):
+        textbox.tag_add("sel", "1.0", "end")
+        return "break"
+
+
+    def clear_all(self, textbox):
+        textbox.delete("1.0", "end")
+
+
+    def update_deck_info(self, label, message: str, is_warning: bool = False):
+        if message == "":
+            label.grid_forget()
+        else:
+            color = "orange" if is_warning else "red"
+            label.configure(text=message, text_color=color)
+            label.grid(row=1, column=0, pady=(0, 5))
+
+
     def show_changes_popup(self):
         # Get raw deck lists from textboxes
-        old_deck_list_raw = self.textbox_old.get("1.0", "end-1c")
-        new_deck_list_raw = self.textbox_new.get("1.0", "end-1c")
+        deck_list_old_raw = self.textbox_old.get("1.0", "end-1c")
+        deck_list_new_raw = self.textbox_new.get("1.0", "end-1c")
 
-        result = get_deck_changes(old_deck_list_raw, new_deck_list_raw)
-        if result is None:
+        # Parse deck lists
+        need_exit = False
+
+        deck_list_old = None
+        try:
+            deck_list_old, warning_message = parse_deck_list(deck_list_old_raw)
+            self.update_deck_info(self.label_info_old, warning_message, is_warning=True)
+        except DeckParseError as e:
+            self.update_deck_info(self.label_info_old, str(e), is_warning=False)
+            need_exit = True
+
+        deck_list_new = None
+        try:
+            deck_list_new, warning_message = parse_deck_list(deck_list_new_raw)
+            self.update_deck_info(self.label_info_new, warning_message, is_warning=True)
+        except DeckParseError as e:
+            self.update_deck_info(self.label_info_new, str(e), is_warning=False)
+            need_exit = True
+
+        if need_exit:
             return
 
-        changes = result[0]
-        nb_changes = result[1]
-
-        result_text = f"Modifications ({nb_changes}):\n{changes}"
+        # Get changes
+        changes_display, changes_copy = get_deck_changes(deck_list_old, deck_list_new)
 
         # Create popup window
         popup = ctk.CTkToplevel(self)
-        popup.title("Modifications")
+        popup.title("Changes")
         popup.geometry("400x300")
         popup.grab_set() # Make popup modal (block interaction with main window)
         popup.attributes('-topmost', True) # Make popup always on top of the main window
@@ -80,19 +126,31 @@ class DeckUpdaterApp(ctk.CTk):
         # Create popup content
         result_box = ctk.CTkTextbox(popup)
         result_box.grid(row=0, column=0, padx=10, pady=10, sticky="nsew")
-        result_box.insert("1.0", result_text)
+
+        text_widget = result_box._textbox
+        text_widget.tag_config("add", foreground="lightgreen")
+        text_widget.tag_config("sub", foreground="salmon")
+
+        for line in changes_display.split('\n'):
+            if line.startswith('+'):
+                result_box.insert("end", line + '\n', "add")
+            elif line.startswith('-'):
+                result_box.insert("end", line + '\n', "sub")
+            else:
+                result_box.insert("end", line + '\n')
+
         result_box.configure(state="disabled") # Désactive la modification
 
         # Copy button callback
         def copy_to_clipboard():
             self.clipboard_clear()
-            self.clipboard_append(changes)
+            self.clipboard_append(changes_copy)
             btn_copy.configure(text="Copié ! ✔️", fg_color="green")
             # Remet le bouton à son état normal après 2 secondes
-            popup.after(2000, lambda: btn_copy.configure(text="Copier les changements", fg_color=["#3B8ED0", "#1F6AA5"]))
+            popup.after(2000, lambda: btn_copy.configure(text="Copy changes", fg_color=["#3B8ED0", "#1F6AA5"]))
 
         # 5. Copy button
-        btn_copy = ctk.CTkButton(popup, text="Copier les changements", command=copy_to_clipboard)
+        btn_copy = ctk.CTkButton(popup, text="Copy changes", command=copy_to_clipboard)
         btn_copy.grid(row=1, column=0, padx=10, pady=10, sticky="ew")
 
 
--- src/transformations.py
+++ src/transformations.py
@@ -1,6 +1,12 @@
 from define import VALID_DECK_SIZES
 
-def _get_deck_list(deck_list_raw: str) -> list[tuple[int, str]] | None:
+# Dans transformations.py
+class DeckParseError(Exception):
+    pass
+
+def parse_deck_list(deck_list_raw: str) -> list[tuple[int, str]]:
+    warning_message = ""
+
     # Parse deck list
     number_of_cards = 0
     deck_list = []
@@ -28,15 +34,13 @@ def _get_deck_list(deck_list_raw: str) -> list[tuple[int, str]] | None:
         # Get the number of card
         card_number_end = line.find('x ')
         if card_number_end == -1:
-            print(f"Error: Invalid card format on line {line_number + 1}")
-            continue
+            raise DeckParseError(f"Error: Invalid card format on line {line_number + 1}")
 
         card_number = 0
         try:
             card_number = int(line[:card_number_end].strip())
         except ValueError:
-            print(f"Error: Invalid card number on line {line_number + 1}")
-            continue
+            raise DeckParseError(f"Error: Invalid card number on line {line_number + 1}")
 
         number_of_cards += card_number
 
@@ -45,13 +49,12 @@ def _get_deck_list(deck_list_raw: str) -> list[tuple[int, str]] | None:
         deck_list.append((card_number, card_name))
 
     if number_of_cards == 0:
-        print("Error: The deck is empty.")
-        return None
+        raise DeckParseError("Error: The deck is empty")
 
     if number_of_cards not in VALID_DECK_SIZES:
-        print(f"Warning: The deck contains {number_of_cards} cards instead of {VALID_DECK_SIZES}.")
+        warning_message = f"Warning: The deck contains {number_of_cards} cards instead of {VALID_DECK_SIZES}"
 
-    return deck_list
+    return (deck_list, warning_message)
 
 
 def _getCardNumber(deck_list, card_name):
@@ -61,15 +64,11 @@ def _getCardNumber(deck_list, card_name):
     return 0
 
 
-def get_deck_changes(deck_list_old_raw: str, deck_list_new_raw: str) -> tuple[str, int] | None:
-    # Get deck lists
-    deck_list_old = _get_deck_list(deck_list_old_raw)
-    deck_list_new = _get_deck_list(deck_list_new_raw)
-
-    if deck_list_old is None or deck_list_new is None:
-        return None
-
-    number_of_modifications = 0
+def get_deck_changes(
+        deck_list_old: list[tuple[int, str]],
+        deck_list_new: list[tuple[int, str]]
+        ) -> tuple[str, int]:
+    number_of_changes = 0
 
     # Get added cards
     added_cards = []
@@ -77,10 +76,10 @@ def get_deck_changes(deck_list_old_raw: str, deck_list_new_raw: str) -> tuple[st
         card_number_old = _getCardNumber(deck_list_old, card_name)
         if card_number == 0:
             added_cards.append((card_number, card_name))
-            number_of_modifications += 1
+            number_of_changes += 1
         elif card_number > card_number_old:
             added_cards.append((card_number - card_number_old, card_name))
-            number_of_modifications += card_number - card_number_old
+            number_of_changes += card_number - card_number_old
 
     # Get removed cards
     removed_cards = []
@@ -88,25 +87,33 @@ def get_deck_changes(deck_list_old_raw: str, deck_list_new_raw: str) -> tuple[st
         card_number_new = _getCardNumber(deck_list_new, card_name)
         if card_number == 0:
             removed_cards.append((card_number, card_name))
-            number_of_modifications += 1
+            number_of_changes += 1
         elif card_number > card_number_new:
             removed_cards.append((card_number - card_number_new, card_name))
-            number_of_modifications += card_number - card_number_new
+            number_of_changes += card_number - card_number_new
 
-    # Get modifications
-    modifications = ""
+    # Get changes
+    changes_display = f"{number_of_changes} changes :\n"
+    changes_copy = ""
     for card_number, card_name in added_cards:
         if card_number > 1:
-            modifications  += f"+ {card_number} {card_name}\n"
+            changes_display += f"+ {card_number} {card_name}\n"
+            changes_copy += f"+ {card_number} {card_name}\n"
         else:
-            modifications  += f"+ {card_name}\n"
+            changes_display += f"+ {card_name}\n"
+            changes_copy += f"+ {card_name}\n"
 
-    modifications  += "\n"
+    changes_copy += "\n"
 
     for card_number, card_name in removed_cards:
         if card_number > 1:
-            modifications  += f"- {card_number} {card_name}\n"
+            changes_display += f"- {card_number} {card_name}\n"
+            changes_copy += f"- {card_number} {card_name}\n"
         else:
-            modifications  += f"- {card_name}\n"
+            changes_display += f"- {card_name}\n"
+            changes_copy += f"- {card_name}\n"
 
-    return (modifications[:-1], number_of_modifications)
+    changes_display = changes_display[:-1]
+    changes_copy = changes_copy[:-1]
+
+    return (changes_display, changes_copy)
